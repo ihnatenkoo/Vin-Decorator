3e06135bbff4f7f2bb88c7726809f0dae23a7207
Dmytro <ihnatenko.dev@gmail.com>
2022-08-11 15:15:29 +0300

    Add close icon in <SearchPanel/>

--- src/components/SearchPanel/SearchPanel.tsx
+++ src/components/SearchPanel/SearchPanel.tsx
@@ -3,7 +3,7 @@ import React, { FC, useState, KeyboardEvent } from 'react';
 import s from './SearchPanel.module.scss';
 
 const SearchPanel: FC = () => {
-	const [search, setSearch] = useState('');
+	const [searchTerm, setSearchTerm] = useState('');
 
 	const searchHandler = (e: KeyboardEvent<HTMLInputElement>) => {
 		if (e.key === 'Enter') {
@@ -17,10 +17,15 @@ const SearchPanel: FC = () => {
 				type="text"
 				className={s.search__input}
 				placeholder="Enter vehicle VIN code..."
-				value={search}
-				onChange={(e) => setSearch(e.target.value.toUpperCase())}
+				value={searchTerm}
+				onChange={(e) => setSearchTerm(e.target.value.toUpperCase())}
 				onKeyPress={searchHandler}
 			/>
+			{searchTerm && (
+				<button onClick={() => setSearchTerm('')} className={s.search__clear}>
+					<span className="material-icons-outlined">close</span>
+				</button>
+			)}
 		</div>
 	);
 };
